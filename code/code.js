51dba01cb18b07fbe5d7495dea4ded6f218931ac
Camila <itsmyllaa@github.com>
2023-08-10 19:14:47 -0300

    Programação Funcional, Imperativa e Declarativa

---
+++ code/code.js
@@ -0,0 +1,13 @@
+//Imperativa-Declarativa
+
+//Função que eleva o número ao quadrado
+
+//Imperativo: faça da seguinte forma:
+let number = 2
+
+function square(){
+    return number * number
+}
+number = square()
+//Declarativa: o que fazer e como não fazer:
+const square = n => n * n
